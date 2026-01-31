d116ef78f150bf95937302fc901d1ccb9fbe23f9
Reuben Bowlby <reuben@hummbl.io>
2026-01-31 12:12:41 -0500

    feat(router): add deterministic Base120 bindings validator
    Wave: BASE120 router expansion
    
    Validator: shape + duplicates only; transformation validity deferred to kernel-fed set

--- packages/router/dist/router/src/index.js
+++ packages/router/dist/router/src/index.js
@@ -4,4 +4,5 @@ export * from "./selectors";
 export * from "./router";
 export * from "./capabilities";
 export * from "./base120/bindings";
+export * from "./base120/validateBindings";
 //# sourceMappingURL=index.js.map
--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -4,3 +4,4 @@ export * from "./selectors";
 export * from "./router";
 export * from "./capabilities";
 export * from "./base120/bindings";
+export * from "./base120/validateBindings";
